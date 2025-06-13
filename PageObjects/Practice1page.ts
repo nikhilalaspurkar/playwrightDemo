0de6674fc0948127d1a394880f694e7f7793b553
Nikhil-A <alaspurkar05@gmail.com>
2025-06-13 11:58:01 +0530

    added new practice scenario ofr window handle in page practice spce

--- PageObjects/Practice1page.ts
+++ PageObjects/Practice1page.ts
@@ -1,17 +1,33 @@
-import { Locator, Page, test } from "@playwright/test";
+import { expect, Locator, Page, test } from "@playwright/test";
 export class Practicepage{
 
     readonly page: Page;
     readonly radiobutton: Locator;
+    readonly newwindowbutton: Locator;
+    readonly opentab: Locator;
+    readonly accescourse: Locator;
 
     constructor (page:Page)
     {
         this.page= page;
         this.radiobutton= this.page.locator('//input[@value="radio1"]');
+        this.newwindowbutton= this.page.locator("#openwindow");
+        this.accescourse= this.page.locator('//div[@class="button float-left"]');
+        this.opentab= this.page.locator('#openwindow');
     }
 
     async radiobuttonclick()
     {
         await this.radiobutton.click();
+        await expect(this.page.getByText("Practice Page")).toBeVisible();
+    }
+    async newwindowyab()
+    {
+        await this.newwindowbutton.click();
+        this.page.waitForEvent;
+        // const newpage= await pagePromise;
+        console.log(await this.page.title());
+        await this.accescourse.click();
+
     }
 }
--- tests/Practice1.spec.ts
+++ tests/Practice1.spec.ts
@@ -1,15 +1,28 @@
-import test from "@playwright/test";
-import { Practicepage} from "../PageObjects/Practice1page";
-test.describe("rahul shetty academy", () =>{
+import test, { expect } from "@playwright/test";
+import { Practicepage } from "../PageObjects/Practice1page";
+test.describe("rahul shetty academy", () => {
 
-test.beforeEach("launch the rahul shetty", async ({page})=>{
-    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
-})
+    test.beforeEach("launch the rahul shetty", async ({ page }) => {
+        await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    })
 
-test ("click on radio button", async ({page})=> {
-//radio button method
-const practicepage= new Practicepage(page);
-await practicepage.radiobuttonclick();
-})
+    test("click on radio button", async ({ page }) => {
+        //radio button method
+        const practicepage = new Practicepage(page);
+        await practicepage.radiobuttonclick();
+    })
+
+    test.only("handle new window", async ({ page }) => {
+        const [newtab] = await Promise.all([
+            page.waitForEvent("popup"),
+            page.locator('#openwindow').click(),
+        ]);
+
+        await newtab.waitForLoadState("load");
+
+        let allcoursebuton = newtab.locator('//a[contains(text(),"Access all our Courses")]');
+        await expect(allcoursebuton).toBeVisible();
+
+    })
 
 })
